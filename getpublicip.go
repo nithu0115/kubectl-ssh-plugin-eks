3d6c1830a291fb72572384882ec51d6b62d8240d
EC2 Default User <ec2-user@ip-10-0-1-30.us-west-2.compute.internal>
2018-09-04 04:02:57 +0000

    Adding support to SSH into Worker Nodes with private IP

--- getpublicip.go
+++ getpublicip.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"flag"
+	"strings"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -35,10 +36,128 @@ func main() {
 		}
 		return
 	}
-	//fmt.Println(result)
+
+	subnetID, RouteTableResponse, err, PrivateDnsName, PublicDnsName := descrInstance(ec2svc, result)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	isPublic, err := isSubnetPublic(RouteTableResponse, subnetID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if !isPublic {
+		fmt.Println("PrivateDnsName",PrivateDnsName)
+	} else {
+		fmt.Println("PublicDnsName",PublicDnsName)
+	}
+}
+
+func descrInstance(ec2svc *ec2.EC2, result *ec2.DescribeInstancesOutput) (subnetID string, RouteTableResponse []*ec2.RouteTable ,err error, PrivateDnsName string, PublicDnsName string ) {
 	for idx, _ := range result.Reservations {
-		for _, publicip := range result.Reservations[idx].Instances{
-			fmt.Println (*publicip.PublicIpAddress)
+		for _, isSubnetPublic := range result.Reservations[idx].Instances{
+			subnetID := *isSubnetPublic.SubnetId
+			subnetRequest := &ec2.DescribeSubnetsInput{
+				SubnetIds: []*string{
+					&subnetID,
+				},
+			}
+			subnetResponse, err := ec2svc.DescribeSubnets(subnetRequest)
+			if err != nil {
+				if aerr, ok := err.(awserr.Error); ok {
+					switch aerr.Code() {
+					default:
+						fmt.Println(aerr.Error())
+					}
+				} else {
+					// Print the error, cast err to awserr.Error to get the Code and
+					// Message from an error.
+					fmt.Println(err.Error())
+				}
+				//return
+			}
+
+			vpcId := subnetResponse.Subnets[0].VpcId
+			//vpcId := subnetResponse.Subnets
+
+			routeTableRequest := &ec2.DescribeRouteTablesInput{
+				Filters: []*ec2.Filter{
+					  {
+						Name: aws.String("vpc-id"),
+						Values: []*string{
+						     aws.String(*vpcId),
+					      },
+				      },
+				      /*{
+						Name: aws.String("association.subnet-id"),
+						Values: []*string{
+						     aws.String(subnetID),
+					      },
+				      },*/
+				  },
+			   }
+			routeTableresponse, err := ec2svc.DescribeRouteTables(routeTableRequest)
+			if err != nil {
+				if aerr, ok := err.(awserr.Error); ok {
+					switch aerr.Code() {
+					default:
+						fmt.Println(aerr.Error())
+					}
+				} else {
+					// Print the error, cast err to awserr.Error to get the Code and
+					// Message from an error.
+					fmt.Println(err.Error())
+				}
+				//return
+			}
+			RouteTableResponse := routeTableresponse.RouteTables
+			return subnetID, RouteTableResponse, err, *isSubnetPublic.PrivateDnsName, *isSubnetPublic.PublicIpAddress
 		}
 	}
+	return
+}
+
+func isSubnetPublic(RouteTableResponse []*ec2.RouteTable, subnetID string) (bool, error) {
+	var subnetTable *ec2.RouteTable
+	for _, table := range RouteTableResponse {
+		for _, assoc := range table.Associations {
+			if aws.StringValue(assoc.SubnetId) == subnetID {
+				subnetTable = table
+				break
+			}
+		}
+	}
+
+	if subnetTable == nil {
+		// If there is no explicit association, the subnet will be implicitly
+		// associated with the VPC's main routing table.
+		for _, table := range RouteTableResponse {
+			for _, assoc := range table.Associations {
+				if aws.BoolValue(assoc.Main) == true {
+					//fmt.Println("Assuming implicit use of main routing table %s for %s",
+					//	aws.StringValue(table.RouteTableId), subnetID)
+					subnetTable = table
+					break
+				}
+			}
+		}
+	}
+
+	if subnetTable == nil {
+		return false, fmt.Errorf("Could not locate routing table for subnet %s", subnetID)
+	}
+
+	for _, route := range subnetTable.Routes {
+		// There is no direct way in the AWS API to determine if a subnet is public or private.
+		// A public subnet is one which has an internet gateway route
+		// we look for the gatewayId and make sure it has the prefix of igw to differentiate
+		// from the default in-subnet route which is called "local"
+		// or other virtual gateway (starting with vgv)
+		// or vpc peering connections (starting with pcx).
+		if strings.HasPrefix(aws.StringValue(route.GatewayId), "igw") {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
--- script_ssh.sh
+++ script_ssh.sh
@@ -72,7 +72,29 @@ else [ "$KUBECTL_PLUGINS_LOCAL_FLAG_CLOUD_PROVIDER" == "aws" ]
   instanceID=$(kubectl get node $1 -o jsonpath="${path}" $selector)
   region=$(kubectl get node $1 -o jsonpath="${pathregion}" $selector | awk -F/ '{print $4}')
   get_publicIP
-  publicIP=`${getpublicip} -region=${region%?} -instanceid=$instanceID`
-  echo "${publicIP}"
-  [[ ! -z "${publicIP}" ]] && ssh -i ${KUBECTL_PLUGINS_LOCAL_FLAG_IDENTITY_FILE} ${KUBECTL_PLUGINS_LOCAL_FLAG_SSH_USER}@$publicIP || echo "Can't SSH"; exit 1;
+  BOTH_IP_DNS=`${getpublicip} -region=${region%?} -instanceid=$instanceID`
+  DNS_TYPE=`echo "${BOTH_IP_DNS}" | awk -F " " '{print $1}'`
+  echo $DNS_TYPE
+  IP=`echo "${BOTH_IP_DNS}" | awk -F " " '{print $2}'`
+  if [ "${DNS_TYPE}" == "PrivateDnsName" ] && [ "${KUBECTL_PLUGINS_LOCAL_FLAG_IDENTITY_FILE}" == "" ]
+  then
+  echo "Worker Nodes does not have IGW, do you wish to continue to SSHing into ${IP} ?"
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) [[ ! -z "${IP}" ]] && ssh ${KUBECTL_PLUGINS_LOCAL_FLAG_SSH_USER}@$IP || echo "Can't SSH"; exit 1;;
+        NO ) exit;;
+      esac
+    done
+  elif [ "${DNS_TYPE}" == "PrivateDnsName" ]
+  then
+  echo "Worker Nodes does not have IGW, do you still want to SSH ?"
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) [[ ! -z "${IP}" ]] && ssh -i ${KUBECTL_PLUGINS_LOCAL_FLAG_IDENTITY_FILE} ${KUBECTL_PLUGINS_LOCAL_FLAG_SSH_USER}@$IP || echo "Can't SSH"; exit 1;;
+        NO ) exit;;
+      esac
+    done
+  else
+    [[ ! -z "${IP}" ]] && echo "SSHing into Worker Node with IP: ${IP}" && ssh -i ${KUBECTL_PLUGINS_LOCAL_FLAG_IDENTITY_FILE} ${KUBECTL_PLUGINS_LOCAL_FLAG_SSH_USER}@$IP || echo "Can't SSH"; exit 1;
+  fi
 fi
